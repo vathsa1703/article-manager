3766e24d7af31973dddf3ac6f148d9210deb2d90
devan <devan.prgt@gmail.com>
2026-06-09 18:33:29 +0200

    refactor(backend): move test_per_user_isolation to test_auth

--- backend/src/tests/test_api.py
+++ backend/src/tests/test_api.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tests.conftest import INVALID_ARTICLE_CASES, get_csrf_header
+from tests.conftest import INVALID_ARTICLE_CASES
 
 
 def test_health(client):
@@ -113,43 +113,6 @@ def test_top_authors(auth_client, create_list_authors_articles):
     for i in range(len(expected_responses)):
         assert payload[i]["author"] == expected_responses[i][0]
         assert payload[i]["count"] == expected_responses[i][1]
-
-
-def test_per_user_isolation(client, mock_article):
-    # User A
-    res1 = client.post("/auth/register", json={"name": "Test", "password": "Test"})
-    assert res1.status_code == 201
-    headers1 = get_csrf_header(res1, "access")
-
-    post1 = client.post("/articles", json=mock_article, headers=headers1)
-    assert post1.status_code == 201
-    payload1 = client.get("/articles", headers=headers1).get_json()
-    assert len(payload1) == 1
-    article_id = post1.get_json()["id"]
-
-    # User B
-    res2 = client.post("/auth/register", json={"name": "Test 2", "password": "Test 2"})
-    assert res2.status_code == 201
-    headers2 = get_csrf_header(res2, "access")
-
-    # User B cannot see User A articles
-    assert len(client.get("/articles", headers=headers2).get_json()) == 0
-
-    # User B cannot modify/delete User A articles
-    edited = {**mock_article, "id": article_id, "title": "hacked"}
-    assert client.put("/articles", json=edited, headers=headers2).status_code == 404
-    assert (
-        client.delete(
-            "/articles", json={"ids": [article_id]}, headers=headers2
-        ).status_code
-        == 404
-    )
-
-    # A still has article
-    res3 = client.post("/auth/login", json={"name": "Test", "password": "Test"})
-    assert res3.status_code == 200
-    headers3 = get_csrf_header(res3, "access")
-    assert len(client.get("/articles", headers=headers3).get_json()) == 1
 
 
 def test_duplicated_url(auth_client, article, mock_article_2):
--- backend/src/tests/test_auth.py
+++ backend/src/tests/test_auth.py
@@ -51,3 +51,40 @@ def test_session_verified(auth_client):
 def test_session_rejected(client):
     res = client.get("/auth/session")
     assert res.status_code == 401
+
+
+def test_per_user_isolation(client, mock_article):
+    # User A
+    res1 = client.post("/auth/register", json={"name": "Test", "password": "Test"})
+    assert res1.status_code == 201
+    headers1 = get_csrf_header(res1, "access")
+
+    post1 = client.post("/articles", json=mock_article, headers=headers1)
+    assert post1.status_code == 201
+    payload1 = client.get("/articles", headers=headers1).get_json()
+    assert len(payload1) == 1
+    article_id = post1.get_json()["id"]
+
+    # User B
+    res2 = client.post("/auth/register", json={"name": "Test 2", "password": "Test 2"})
+    assert res2.status_code == 201
+    headers2 = get_csrf_header(res2, "access")
+
+    # User B cannot see User A articles
+    assert len(client.get("/articles", headers=headers2).get_json()) == 0
+
+    # User B cannot modify/delete User A articles
+    edited = {**mock_article, "id": article_id, "title": "hacked"}
+    assert client.put("/articles", json=edited, headers=headers2).status_code == 404
+    assert (
+        client.delete(
+            "/articles", json={"ids": [article_id]}, headers=headers2
+        ).status_code
+        == 404
+    )
+
+    # A still has article
+    res3 = client.post("/auth/login", json={"name": "Test", "password": "Test"})
+    assert res3.status_code == 200
+    headers3 = get_csrf_header(res3, "access")
+    assert len(client.get("/articles", headers=headers3).get_json()) == 1
